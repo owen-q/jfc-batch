e6fd5499b84f27d91e0cf6c8e99a21cf215c38ea
owen-q <owen.q.dev@gmail.com>
2018-07-20 12:53:56 +0900

    d

---
+++ src/main/java/io/owen/jfc/batch/job/match/JobConfiguration.java
@@ -0,0 +1,35 @@
+package io.owen.jfc.batch.job.match;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by owen_q on 2018. 7. 20..
+ */
+@Configuration
+@EnableBatchProcessing
+public class JobConfiguration {
+    private Logger logger = LoggerFactory.getLogger(JobConfiguration.class);
+
+    private static final String JOB_NAME = "match-generate";
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private Step step;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(step)
+                .build();
+    }
+}
---
+++ src/main/java/io/owen/jfc/batch/job/match/StepConfiguration.java
@@ -0,0 +1,84 @@
+package io.owen.jfc.batch.job.match;
+
+import io.owen.jfc.common.entity.Match;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.DayOfWeek;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
+/**
+ * Created by owen_q on 2018. 7. 20..
+ */
+@Configuration
+public class StepConfiguration {
+    private Logger logger = LoggerFactory.getLogger(StepConfiguration.class);
+
+    private static final String STEP_NAME = "StepName";
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step Step() {
+        return stepBuilderFactory.get(STEP_NAME)
+                .<ZonedDateTime, Match>chunk(1)
+                .reader(sampleItemReader())
+                .processor(sampleItemProcessor())
+                .writer(sampleItemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<ZonedDateTime> sampleItemReader() {
+
+        ItemReader<ZonedDateTime> matchDateReader = ()->{
+            final String ZONE_ID = "Asia/Seoul";
+
+            ZonedDateTime today = ZonedDateTime.now(ZoneId.of(ZONE_ID));
+            ZonedDateTime matchDate = ZonedDateTime.now(ZoneId.of(ZONE_ID));
+
+            DayOfWeek todayDayOfWeek = today.getDayOfWeek();
+
+            matchDate = matchDate.plusDays(DayOfWeek.SUNDAY.minus(todayDayOfWeek.ordinal()).ordinal()).withHour(10);
+            return matchDate;
+        };
+
+        return matchDateReader;
+    }
+
+    @Bean
+    public ItemProcessor<ZonedDateTime, Match> sampleItemProcessor() {
+        return (matchDate) ->{
+
+
+            Match match = new Match();
+            match.setAttendList(new ArrayList<>());
+            match.setMatchDate(matchDate);
+
+            return match;
+        };
+    }
+
+    @Bean
+    public JpaItemWriter<Match> sampleItemWriter() {
+        JpaItemWriter<Match> writer = new JpaItemWriter<>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+
+        return writer;
+    }
+}
